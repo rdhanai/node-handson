8e8836b2e279c6e74f008068cfbd8daaa709a1d4
Rajpal Singh <rdhanai@innominds.com>
2018-10-07 17:38:45 +0530

    modified for port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -46,6 +47,6 @@ app.get('/about', (request, response)=> {
     });
 });
 
-app.listen(3000, ()=>{
-    console.log('server is up on port 3000');
+app.listen(port, ()=>{
+    console.log(`server is up on port ${port}`);
 });
