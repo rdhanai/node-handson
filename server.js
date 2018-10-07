8f3af91eb39b34bacec54a02cf225ddd51eb0acc
Rajpal Singh <rdhanai@innominds.com>
2018-10-07 16:47:43 +0530

    first commit...

---
+++ server.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', ()=> {
+    return new Date().getFullYear();
+})
+hbs.registerHelper('screamIt', (text)=> {
+    return text.toUpperCase();
+})
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname+'/public'));
+
+app.use ( (req, res, next) => {
+    var now = new Date().toString();
+    console.log('middleware... ', now);
+    next();
+});
+
+app.use((request, response, next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${request.method} ${request.url}`;
+    console.log('log...', log);
+    fs.appendFile('./logs/server.log', log + '\n', ()=> {});
+    next();
+});
+
+// app.use((request, response, next)=>{
+//     response.render('maintenance.hbs', {});
+// });
+
+
+app.get('/', (request, response)=> {
+    response.render('home.hbs', {
+        pageTitle: 'Home Page',
+        message: 'welecome to my app'
+    });
+});
+app.get('/about', (request, response)=> {
+    response.render('about.hbs', {
+        pageTitle: 'About Page Heading'
+    });
+});
+
+app.listen(3000, ()=>{
+    console.log('server is up on port 3000');
+});
